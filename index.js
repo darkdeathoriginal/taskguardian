11e3699681493d15d6cbc3330ae52d79b2dcb7bc
Anwin Sharon <87226858+darkdeathoriginal@users.noreply.github.com>
2024-03-30 21:46:03 +0530

    swagger fix test

--- index.js
+++ index.js
@@ -31,7 +31,17 @@ const options = {
 };
 
 const specs = swaggerJsdoc(options);
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs,{
+  customJs: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ]
+}));
 app.get("/", (req, res) => {
   res.redirect("/docs");
 });
